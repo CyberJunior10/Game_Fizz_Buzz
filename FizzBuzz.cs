c928a83f4e65193bf0b7bed1448c5bc8639f62a3
Pawel Skrzypczynski <100528749@unimail.derby.ac.uk>
2021-06-28 13:56:03 +0100

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Game_Fizz_Buzz
+{
+    class Game_Fizz_Buzz
+    {
+        static void Main()
+        {
+            int dividedWithTree = 3;
+            int dividedWithFive = 5;
+
+            while (true)
+            {
+                Console.WriteLine("\nWelcome in Game Fizz Buzz !!!.\n");
+                try
+                {
+                    Console.WriteLine("\nGive a number: \n");
+                    var userNumber = int.Parse(Console.ReadLine());
+
+                    if ((userNumber % dividedWithTree == 0) && (userNumber % dividedWithFive == 0))
+                    {
+                        Console.WriteLine("\n***  !!! FizzBuzz !!! ***, {0} is lucky number !.", userNumber);
+                        break;
+                    }
+
+                    else if (userNumber % dividedWithTree == 0)
+                    {
+                        Console.WriteLine("\n--- Fizz ! ---. Close to lucky number.");
+                    }
+                    else if (userNumber % dividedWithFive == 0)
+                    {
+                        Console.WriteLine("\n--- Buzz ! --->. Close to lucky number.");
+                    }
+                    else //(userNumber !% dividedWithTree == 0) && (userNumber !% dividedWithFive == 0))
+                    {
+                        Console.WriteLine("\nInput number: {0}. Far to lucky number.", userNumber);
+                    }
+                }
+                catch (ArithmeticException)
+                {
+                    Console.WriteLine("Wrong data input.");
+                }
+            }
+        }
+    }
+   
+}
